61f9a44fcfaeb4316b0c09adf553375c21d61867
Jubert_Nantes <jubert.nantes0418@gmail.com>
2026-01-01 23:18:44 +0800

    update

--- react-portfolio/src/components/sections/Projects/Project.tsx
+++
@@ -1,9 +0,0 @@
-import ErrorHandling from '../others/ErrorHandling'
-
-export default function Project() {
-  return (
-    <div className="flex items-center justify-center h-[45vw]">
-        <ErrorHandling />
-    </div>
-  )
-}
