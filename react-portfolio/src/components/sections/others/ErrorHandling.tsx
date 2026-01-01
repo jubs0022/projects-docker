549114a1d1c966e9944d1ea9203d20e9776d1099
Jubert_Nantes <jubert.nantes0418@gmail.com>
2026-01-02 00:08:53 +0800

    Deleted File

--- react-portfolio/src/components/sections/others/ErrorHandling.tsx
+++
@@ -1,9 +0,0 @@
-export default function ErrorHandling(){
-    return(
-        <div className="max-w-[1440px] flex flex-col items-center mx-auto font-Poppins">
-            <h1 className="font-extrabold 2xl:text-[50px]">501</h1>
-            <h3 className="font-bold 2xl:text-[30px]">Not Implemented</h3>
-            <p className="font-normal 2xl:text-[16px]">This feature is under development</p>
-        </div>
-    );
-}
